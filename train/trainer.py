fdbda28a92a5497539b54a7cae70ab437f5c5cde
Amir Sharifi <am.sharifi1995@gmail.com>
2022-07-02 15:00:30 +0430

    feat(train): Add CLI for using in terminal #5

--- train/trainer.py
+++ train/trainer.py
@@ -1,98 +1,84 @@
-from torchmetrics import F1, Accuracy, Precision, Recallimport
-
-import torch
 import os
-import shutil
+
+import click
 import flash
+import torch
 from flash.image import ImageClassifier
-from utils import dataloader, utils, logger, augment
+from torchmetrics import Accuracy, Precision, Recall
+from torchmetrics.classification.f_beta import F1Score
+
+from utils import dataloader, logger
 
 
 class ClassificationTrainer:
-    def __init__(self, backbone, pre_trained_path=None, is_augment=False, augment_params=None, epochs=100, batch_size=4,
-                 num_dataloader_workers=8, num_classes=2):
+    def __init__(self, backbone='mobilenet_v2', pre_trained_path=None, epochs=100, batch_size=2,
+                 num_dataloader_workers=8, image_width=256, image_height=256,
+                 validation_split=0.2):
         self.backbone = backbone
-        self.is_augment = is_augment
-        self.augment_params = augment_params
         self.pre_trained_path = pre_trained_path
         self.epochs = epochs
         self.batch_size = batch_size
         self.num_dataloader_workers = num_dataloader_workers
-        self.num_classes = num_classes
+        self.image_width = image_width
+        self.image_height = image_height
+        self.validation_split = validation_split
 
-        pass
-
-    def train_from_images(self, train_path, val_path, save_name):
-
-        datamodule = dataloader.get_dataset_for_flash(train_path, val_path, self.batch_size,
-                                                      self.num_dataloader_workers)
-
-        if self.pre_trained_path != None:
+    def trainer(self, dataset_path, save_name=''):
+        datamodule = dataloader.get_dataset_for_flash(dataset_path, self.batch_size,
+                                                      self.num_dataloader_workers,
+                                                      image_width=self.image_width,
+                                                      image_height=self.image_height,
+                                                      split=self.validation_split)
+        if self.pre_trained_path:
             model = ImageClassifier.load_from_checkpoint(self.pre_trained_path)
         else:
             model = ImageClassifier(backbone=self.backbone,
                                     labels=datamodule.labels,
-                                    num_classes=self.num_classes,
+                                    num_classes=datamodule.num_classes,
                                     metrics=[
-                                        F1(num_classes=datamodule.num_classes,
-                                           mdmc_average='samplewise'),
-                                        Accuracy(num_classes=datamodule.num_classes,
-                                                 mdmc_average='samplewise'),
-                                        Precision(num_classes=datamodule.num_classes,
-                                                  mdmc_average='samplewise'),
-                                        Recall(num_classes=datamodule.num_classes,
-                                               mdmc_average='samplewise')]
+                                        F1Score(num_classes=datamodule.num_classes),
+                                        Accuracy(num_classes=datamodule.num_classes),
+                                        Precision(num_classes=datamodule.num_classes),
+                                        Recall(num_classes=datamodule.num_classes)]
                                     )
 
         trainer = flash.Trainer(
             max_epochs=self.epochs, logger=logger.ClientLogger(), gpus=torch.cuda.device_count())
+        trainer = flash.Trainer(
+            max_epochs=self.epochs, gpus=torch.cuda.device_count())
         trainer.finetune(model, datamodule=datamodule, strategy="no_freeze"),
-        trainer.save_checkpoint(
-            os.path.join(os.environ.get('WEIGHTS_DIR', '/weights'), "{}_model.pt".format(save_name)))
+        save_path = os.path.join(os.environ.get('WEIGHTS_DIR', './weights'), "{}_model.pt".format(save_name))
+        trainer.save_checkpoint(save_path)
         result = trainer.validate(model, datamodule=datamodule)
+        result[0]['model_p'] = save_path
+        return result[0]['model_p']
 
-        return result[0]
 
-    def pre_process_train(self, images_path, validation_split=0.2, save_name=''):
-        train_path, val_path = utils.split_to_train_val(images_path, validation_split)
+@click.command()
+@click.option('--dataset_path', default='./dataset/', help='Path to dataset')
+@click.option('--save_name', default='', help='Name of the model')
+@click.option('--epochs', default=100, help='Number of epochs')
+@click.option('--batch_size', default=2, help='Batch size')
+@click.option('--num_dataloader_workers', default=8, help='Number of dataloader workers')
+@click.option('--image_width', default=64, help='Image width')
+@click.option('--image_height', default=64, help='Image height')
+@click.option('--validation_split', default=0.2, help='Validation split')
+@click.option('--backbone', default='mobilenet_v2', help='Backbone')
+@click.option('--pre_trained_path', default=None, help='Path to pre-trained model')
+def cli(dataset_path, save_name, epochs, batch_size, num_dataloader_workers, image_width, image_height,
+        validation_split, backbone, pre_trained_path):
+    trainer = ClassificationTrainer(backbone=backbone,
+                                    pre_trained_path=pre_trained_path,
+                                    epochs=epochs,
+                                    batch_size=batch_size,
+                                    num_dataloader_workers=num_dataloader_workers,
+                                    image_width=image_width,
+                                    image_height=image_height,
+                                    validation_split=validation_split)
+    result = trainer.trainer(dataset_path, save_name)
+    print(result)
+    return result
 
-        # create temp folder and if exist delete it and create a new one
-        if os.path.exists('temp'):
-            shutil.rmtree('temp')
-        os.mkdir('temp')
 
-        # create train and val folder in temp
-        os.mkdir('temp/train')
-        os.mkdir('temp/val')
-
-        # copy train and val images to train and val folder
-        utils.copy(train_path, 'temp/train')
-        utils.copy(val_path, 'temp/val')
-
-        train_path = 'temp/train'
-        val_path = 'temp/val'
-
-        if self.is_augment:
-            train_path_augmented = 'temp/train_augmented'
-            val_path_augmented = 'temp/val_augmented'
-
-            # Augment train
-            aug = augment.Augmentor(train_path, train_path_augmented, self.augment_params)
-            for _class in os.listdir(train_path):
-                each_class_path = os.path.join(train_path_augmented, _class)
-                os.mkdir(each_class_path)
-                aug.auto_augment(self.augment_params)
-
-            # Augment val
-            aug = augment.Augmentor(val_path, val_path_augmented, self.augment_params)
-            for _class in os.listdir(val_path):
-                each_class_path = os.path.join(val_path_augmented, _class)
-                os.mkdir(each_class_path)
-                aug.auto_augment(self.augment_params)
-
-            train_path = train_path_augmented
-            val_path = val_path_augmented
-
-        # train and val images are ready
-        result = self.train_from_images(train_path, val_path, save_name)
-        return result
+if __name__ == '__main__':
+    cli()
