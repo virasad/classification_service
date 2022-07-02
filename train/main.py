631560b7173f27b3406618598b5e144c82ae15bb
Amir Sharifi <am.sharifi1995@gmail.com>
2022-07-02 15:00:09 +0430

    feat(train): Change Train API for easier use

--- train/main.py
+++ train/main.py
@@ -1,54 +1,74 @@
+import os
+from enum import Enum
+from typing import Optional
+
 import requests
-import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
-import os
+
 import trainer as tr
+import torch
 
 app = FastAPI()
 
 
+# TODO add more models
+class ModelName(str, Enum):
+    mobilenet_v2 = "mobilenet_v2"
+    todo1 = 'todo1'
+    todo2 = 'too2'
+
+
 class ClassificationTrain(BaseModel):
-    images : str
-    save_name : Optional[str] = None
-    batch_size : Optional[str] = None
-    extra_kwargs : Optional[dict] = None
-    num_dataloader_workers : Optional[str] = None
-    epochs : Optional[str] = None
-    num_classes : str = 2
-    validation_split : str = 0.2
-    pretrained_path : Optional[str] = None
-    is_augment : Optional[bool] = None
-    augment_params : Optional[dict] = None
-    backbone : Optional[str] = None
+    dataset_p: str
+    save_name: Optional[str] = ''
+    batch_size: Optional[str] = 4
+    extra_kwargs: Optional[dict] = None
+    num_dataloader_workers: Optional[str] = 8
+    epochs: Optional[str] = 100
+    validation_split: str = 0.2
+    pretrained_path: Optional[str] = None
+    backbone: Optional[ModelName] = ModelName.mobilenet_v2.value
+    image_width: Optional[int] = 256
+    image_height: Optional[int] = 256
+
 
 @app.post("/train/")
-def train(classification : ClassificationTrain):
+def train(dataset_p: str,
+          save_name: Optional[str] = '',
+          batch_size: Optional[int] = 4,
+          num_dataloader_workers: Optional[int] = 8,
+          epochs: Optional[int] = 100,
+          validation_split: float = 0.2,
+          pretrained_path: Optional[str] = None,
+          backbone: Optional[ModelName] = ModelName.mobilenet_v2.value,
+          image_width: Optional[int] = 256,
+          image_height: Optional[int] = 256,
+          response_url: Optional[str] = None,
+          extra_kwargs: Optional[dict] = None,
+          ):
     try:
         trainer = tr.ClassificationTrainer(
-            backbone=classification.backbone,
-            pre_trained_path=classification.pretrained_path,
-            is_augment=classification.is_augment,
-            augment_params=classification.augment_params,
-            epochs=classification.epochs,
-            batch_size=classification.batch_size,
-            num_dataloader_workers=classification.num_dataloader_workers,
-            num_classes=classification.num_classes)
+            backbone=backbone,
+            pre_trained_path=pretrained_path,
+            epochs=epochs,
+            batch_size=batch_size,
+            num_dataloader_workers=num_dataloader_workers,
+            image_width=image_width,
+            image_height=image_height,
+            validation_split=validation_split
+        )
 
-        result = trainer.pre_process_train(
-            images_path=classification.images,
-            validation_split=classification.validation_split,
-            save_name=classification.save_name)
-
-        response_url = os.environ.get('RESPONSE_URL', 'http://127.0.0.1:8000/api/v1/train/done')
-        a = requests.post(response_url,
-                          data={**result, **train.extra_kwargs, 'save_name': train.save_name + '_model.pt'})
+        result = trainer.trainer(
+            dataset_path=dataset_p,
+            save_name=save_name)
+        # release GPU memory
+        del trainer
+        torch.cuda.empty_cache()
+        if response_url:
+            requests.post(response_url,
+                              data={**result, **extra_kwargs, 'save_name': train.save_name + '_model.pt'})
         return result
 
     except Exception as e:
         return {"result": "failed", 'error': str(e)}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=int(os.environ.get('PORT', '5554')))
